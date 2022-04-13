2321f6725cd8219c1dbe7e6a4e43ca72374c5a3d
Theo Gainey <theo@heysparkbox.com>
2022-04-13 09:20:49 -0400

    chore: format boilerplate
    format boilerplate to adhere to eslint rules
    
    On branch chore--eslint-config
     Changes to be committed:
    	modified:   pages/_app.js
    	modified:   pages/api/hello.js
    	renamed:    pages/index.js -> pages/index.jsx

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.scss'
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // _app.js has special rules, {...pageProps} cannont be modified. disable eslint to allow
+  // eslint-disable-next-line
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
