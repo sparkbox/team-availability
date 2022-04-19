a50273cd9bdf244bdc46da0fe0566c4db9afc815
Meg Howe <meg@heysparkbox.com>
2022-04-19 09:09:57 -0700

    chore: add more html structure

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,18 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    'Hello World!'
+    <div>
+      <Head>
+        <title>Sparkbox Team Availability</title>
+        <meta name="description" content="See which Sparkboxers are available for project work for a given week." />
+      </Head>
+
+      <main>
+        <h1>
+          Hello World!
+        </h1>
+      </main>
+    </div>
   );
 }
