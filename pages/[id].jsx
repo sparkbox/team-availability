1c7343a085f094f973de84147c993ef3784f38f1
Osmond <osmond@heysparkbox.com>
2022-04-20 09:42:38 -0600

    feat: Fetch data from mock data and render in app
    - Render first and last name of every team member on Home page
    - Render first and last name of relevant team member on Details page
    
    References 163

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,19 +1,40 @@
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 
+import data from '../lib/data';
+
 export default function DetailPage() {
+  const [details, setDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const { id } = router.query;
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    data.fetchDetails(id)
+      .then((result) => {
+        setDetails(result);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }, [id]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!details) return <p>The team member could not be found.</p>;
 
   return (
     <div>
       <Head>
-        <title>{`${id} | Sparkbox Team Availability`}</title>
-        <meta name="description" content={`View details about ${id}, including their projects, skills, and interests.`} />
+        <title>{`${details.firstName} | Sparkbox Team Availability`}</title>
+        <meta name="description" content={`View details about ${details.firstName}, including their projects, skills, and interests.`} />
       </Head>
       <main>
         <h1>
-          {`Hello ${id}!`}
+          {`Greetings ${details.firstName}${details.lastName && ` ${details.lastName}`}!`}
         </h1>
       </main>
     </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 
+import data from '../lib/data';
+
 export default function Home() {
+  const [roster, setRoster] = useState(null);
+  const [rosterIDs, setRosterIDs] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    data.fetchRoster()
+      .then((result) => {
+        setRoster(Object.values(result));
+        setRosterIDs(Object.keys(result));
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!roster) return <p>No team members could be found.</p>;
+
   return (
     <div>
       <Head>
@@ -9,9 +33,13 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>
-          Hello World!
-        </h1>
+        <h1>Week of April 18 to April 25, 2022</h1>
+
+        <ul>
+          {roster.map((member, idx) => (
+            <li key={rosterIDs[idx]}>{`${member.firstName}${member.lastName && ` ${member.lastName}`}`}</li>
+          ))}
+        </ul>
       </main>
     </div>
   );
