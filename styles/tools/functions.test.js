631bf31eb3d80f0480372ecf577e1d7fe7f8601e
Osmond <osmond@heysparkbox.com>
2022-04-26 12:21:55 -0600

    style: Remove linter warning
    References 166

--- styles/tools/functions.test.js
+++ styles/tools/functions.test.js
@@ -6,4 +6,4 @@ import { join } from 'path';
 import { runSass } from 'sass-true';
 
 const sassFile = join(__dirname, 'functions_test.scss');
-runSass({file: sassFile}, {describe, it});
+runSass({ file: sassFile }, { describe, it });
