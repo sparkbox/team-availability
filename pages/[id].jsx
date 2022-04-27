aa10aa01d63b4e75564f87c55c99aeeed9a946f6
Osmond <osmond@heysparkbox.com>
2022-04-27 11:12:38 -0600

    feat: `[id].jsx` redirects to 404 page if team member not found
    - use `getServerSideProps` function to pre-render page
    
    References 173

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,58 +1,46 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
 
 import apiService from '../services/apiService';
-import LoadingStatus, { loadingStates } from '../components/LoadingStatus';
 
-export default function DetailPage() {
-  const [teamMemberDetails, setTeamMemberDetails] = useState({});
-  const [loadingStatus, setLoadingStatus] = useState(loadingStates.EMPTY);
-  const router = useRouter();
-  const { id } = router.query;
-
-  useEffect(() => {
-    const getAndSetData = async () => {
-      try {
-        setLoadingStatus(loadingStates.LOADING);
-        const fetchedTeamMember = await apiService.getTeamMemberById('api/fellowship/', id);
-        setTeamMemberDetails(fetchedTeamMember);
-        setLoadingStatus(loadingStates.LOADED);
-      } catch (err) {
-        setLoadingStatus(loadingStates.ERROR);
-      }
-    };
-
-    if (id) getAndSetData();
-  }, [id]);
-
+export default function DetailPage({ fetchedTeamMember }) {
   return (
     <div>
       <Head>
         <title>
-          {teamMemberDetails && teamMemberDetails.firstName}
+          {fetchedTeamMember.firstName}
           {' '}
           | Sparkbox Team Availability
         </title>
-        <meta name="description" content={`View details about ${teamMemberDetails && teamMemberDetails.firstName}, including their projects, skills, and interests.`} />
+        <meta name="description" content={`View details about ${fetchedTeamMember && fetchedTeamMember.firstName}, including their projects, skills, and interests.`} />
       </Head>
       <main>
-
-        <LoadingStatus
-          loadingStatus={loadingStatus}
-        />
-
-        {loadingStatus === loadingStates.LOADED && (
-          <h1>
-            Greetings
-            {' '}
-            {teamMemberDetails.firstName}
-            {teamMemberDetails.lastName && ` ${teamMemberDetails.lastName}`}
-            !
-          </h1>
-        )}
-
+        <h1>
+          Greetings
+          {' '}
+          {fetchedTeamMember.firstName}
+          {fetchedTeamMember.lastName && ` ${fetchedTeamMember.lastName}`}
+          !
+        </h1>
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps({ params, req }) {
+  const { id } = params;
+  const { host } = req.headers;
+  const baseUrl = `http://${host}/api/fellowship/`;
+  const fetchedTeamMember = await apiService.getTeamMemberById(baseUrl, id);
+
+  if (!fetchedTeamMember) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      fetchedTeamMember,
+    },
+  };
 }
