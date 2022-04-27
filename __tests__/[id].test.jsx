d884fc5739a5bd021ffc2b28939995a56ee72edf
Osmond <osmond@heysparkbox.com>
2022-04-27 11:13:59 -0600

    test: Update `[id].test.jsx` to reflect changes in DetailPage
    References 173

--- __tests__/[id].test.jsx
+++ __tests__/[id].test.jsx
@@ -1,31 +1,51 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { useRouter } from 'next/router';
 
-import DetailPage from '../pages/[id]';
+import apiService from '../services/apiService';
+import DetailPage, { getServerSideProps } from '../pages/[id]';
 
-jest.mock('next/router');
+const MOCK_TEAM_MEMBER = {
+  firstName: 'Aragorn II',
+  lastName: 'Elessar',
+};
+
+jest.mock('../services/apiService');
+
+apiService.getTeamMemberById = jest.fn(() => Promise.resolve(MOCK_TEAM_MEMBER));
 
 describe('DetailPage', () => {
-  const MOCK_TEAM_MEMBER = {
-    '0001': { firstName: 'Aragorn II', lastName: 'Elessar' },
-  };
-
-  useRouter.mockImplementation(() => (
-    {
-      query: { id: '0001' },
-    }
-  ));
-
-  global.fetch = jest.fn(() => (
-    Promise.resolve({
-      json: () => Promise.resolve(MOCK_TEAM_MEMBER),
-    })
-  ));
-
-  it('renders the expected test', async () => {
-    render(<DetailPage />);
+  it('renders the expected text', async () => {
+    render(
+      <DetailPage fetchedTeamMember={MOCK_TEAM_MEMBER} />,
+    );
 
     await waitFor(() => expect(screen.getByText('Greetings Aragorn II Elessar!')).toBeInTheDocument());
+  });
+});
+
+describe('getServerSideProps', () => {
+  it('calls getMemberById', async () => {
+    const context = {
+      params: {
+        id: '0001',
+      },
+      req: {
+        headers: {
+          host: 'localhost:3000',
+        },
+      },
+    };
+    const response = await getServerSideProps(context);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          fetchedTeamMember: {
+            firstName: 'Aragorn II',
+            lastName: 'Elessar',
+          },
+        },
+      }),
+    );
   });
 });
