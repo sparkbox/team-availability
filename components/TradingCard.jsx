af63e894192db01859c56c7eab7bbbaf803d994b
Theo Gainey <theo@heysparkbox.com>
2022-05-16 16:43:08 -0400

    fix: TradingCard & TradingCardImage hover states
    add text underline hover state to name on TradingCard
    refactor TradingCardImage hover state so that hover state does not apply on details page
    refactor TradingCard so that entire card is clickable
    
    Changes to be committed:
    	modified:   components/TradingCard.jsx
    	modified:   styles/components/_trading-card-image.scss
    	modified:   styles/components/_trading-card.scss

--- components/TradingCard.jsx
+++ components/TradingCard.jsx
@@ -6,21 +6,19 @@ export default function TradingCard({
   photo, forecastedHours, name, jobTitle, id, weeklyCapacity,
 }) {
   return (
-    <div className="cmp-trading-card">
-      <TradingCardImage
-        imageUrl={photo}
-        weeklyCapacity={weeklyCapacity}
-        forecastedHours={forecastedHours}
-      />
-      <div className="cmp-trading-card__hrs-bar">
-        <HoursBar forecastedHours={forecastedHours} weeklyCapacity={weeklyCapacity} />
-      </div>
-      <Link href={`/${id}`} passHref>
-        <a href="replace">
-          <span className="cmp-trading-card__name">{name}</span>
-        </a>
-      </Link>
-      <p className="cmp-trading-card__title">{jobTitle}</p>
-    </div>
+    <Link href={`/${id}`} passHref>
+      <a href="replace" className="cmp-trading-card">
+        <TradingCardImage
+          imageUrl={photo}
+          weeklyCapacity={weeklyCapacity}
+          forecastedHours={forecastedHours}
+        />
+        <div className="cmp-trading-card__hrs-bar">
+          <HoursBar forecastedHours={forecastedHours} weeklyCapacity={weeklyCapacity} />
+        </div>
+        <span className="cmp-trading-card__name">{name}</span>
+        <p className="cmp-trading-card__title">{jobTitle}</p>
+      </a>
+    </Link>
   );
 }
