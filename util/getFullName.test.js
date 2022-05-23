bb6e6a85f564aec7d87c77775b63d11c18ef4065
Osmond <osmond@heysparkbox.com>
2022-05-23 13:32:41 -0600

    test: Update relevant tests
    References 215

--- util/getFullName.test.js
+++ util/getFullName.test.js
@@ -2,28 +2,42 @@ import getFullName from './getFullName';
 
 describe('getFullName', () => {
   it('returns "firstName lastName" if both values are truthy', () => {
-    const teamMember = { firstName: 'Tom', lastName: 'Bombadil' };
+    const teamMember = { firstName: 'Tom', lastName: 'Bombadil', suffix: '' };
     const returnedName = getFullName(teamMember);
 
     expect(returnedName).toEqual('Tom Bombadil');
   });
 
-  it('returns only "firstName" if there is no lastName', () => {
-    const teamMember = { firstName: 'Gollum', lastName: '' };
+  it('returns "firstName lastName suffix" if all three values are truthy', () => {
+    const teamMember = { firstName: 'Aragorn', lastName: 'Elessar', suffix: 'II' };
+    const returnedName = getFullName(teamMember);
+
+    expect(returnedName).toEqual('Aragorn Elessar II');
+  });
+
+  it('returns only "firstName" if there is no lastName and no suffix', () => {
+    const teamMember = { firstName: 'Gollum', lastName: '', suffix: '' };
     const returnedName = getFullName(teamMember);
 
     expect(returnedName).toEqual('Gollum');
   });
 
+  it('returns only "firstName suffix" if there is no lastName and a suffix is present', () => {
+    const teamMember = { firstName: 'Argeleb', lastName: '', suffix: 'II' };
+    const returnedName = getFullName(teamMember);
+
+    expect(returnedName).toEqual('Argeleb II');
+  });
+
   it('returns only "lastName" if there is no firstName', () => {
-    const teamMember = { firstName: '', lastName: 'Oakenshield' };
+    const teamMember = { firstName: '', lastName: 'Oakenshield', suffix: '' };
     const returnedName = getFullName(teamMember);
 
     expect(returnedName).toEqual('Oakenshield');
   });
 
   it('returns "Stranger" if neither firstName nor lastName are truthy', () => {
-    const teamMember = { firstName: '', lastName: '' };
+    const teamMember = { firstName: '', lastName: '', suffix: '' };
     const returnedName = getFullName(teamMember);
 
     expect(returnedName).toEqual('Stranger');
