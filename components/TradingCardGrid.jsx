47ee64d8ffbac6a523df75361e3954f23788c3a8
Osmond <osmond@heysparkbox.com>
2022-05-24 10:05:39 -0600

    feat: Establish Context for filtering
    - `context/FilterContext.jsx` exports:
      - `useFilterContext` custom hook
      - `FilterProvider` custom Provider
    - `FilterProvider` wraps around Overview page
    - TradingCardGrid consumes context through `useFilterContext`
    
    References 228

--- components/TradingCardGrid.jsx
+++ components/TradingCardGrid.jsx
@@ -1,10 +1,26 @@
+import { availabilityOptions, useFilterContext } from '../context/FilterContext';
 import getFullName from '../util/getFullName';
 import TradingCard from './TradingCard';
 
 export default function TradingCardGrid({ teamMembers }) {
+  const { project, roles, availability } = useFilterContext();
+
+  const filteredTeamMembers = teamMembers.filter((member) => {
+    const hasHoursAvailable = member.weeklyCapacity - member.forecastedHours > 0;
+
+    if (!hasHoursAvailable && availability === availabilityOptions.AVAILABLE) return false;
+    if (hasHoursAvailable && availability === availabilityOptions.UNAVAILABLE) return false;
+
+    if (roles.length && !roles.includes(member.jobTitle)) return false;
+
+    if (project !== 'all' && member.project !== project) return false;
+
+    return true;
+  });
+
   return (
     <div className="cmp-trading-card-grid">
-      {teamMembers.map((member) => (
+      {filteredTeamMembers.map((member) => (
         <article
           key={member.id}
         >
