4e4acb2579427a923dd0c00606b716a0195612ac
Osmond <osmond@heysparkbox.com>
2022-05-25 16:19:07 -0600

    feat: Refine CurrentProjects styling on mobile
    References 234

--- components/CurrentProjects.jsx
+++ components/CurrentProjects.jsx
@@ -13,11 +13,12 @@ export default function CurrentProjects({ currentProjects }) {
             scope="row"
             className="cmp-current-projects__table-body-cell-header"
           >
-            {project.project}
+            <span className="cmp-current-projects__header-label">Client</span>
+            {project.client}
           </th>
           <td className="cmp-current-projects__table-body-cell">
-            <span className="cmp-current-projects__label">Client: </span>
-            {project.client}
+            <span className="cmp-current-projects__label">Project: </span>
+            {project.project}
           </td>
           <td className="cmp-current-projects__table-body-cell">
             <span className="cmp-current-projects__label">Start: </span>
@@ -34,7 +35,8 @@ export default function CurrentProjects({ currentProjects }) {
 
   return (
     <section className="cmp-current-projects" aria-labelledby="current-projects">
-      <h2 id="current-projects" className="cmp-current-projects__header cmp-current-projects__header--heading">Current Projects</h2>
+      <h2 id="current-projects" className="cmp-current-projects__header">Current Projects</h2>
+
       <Show when={!currentProjects.length}>
         <p className="cmp-current-projects__status">This team member is not currently on a client project.</p>
       </Show>
@@ -43,8 +45,8 @@ export default function CurrentProjects({ currentProjects }) {
         <table className="cmp-current-projects__table" aria-labelledby="current-projects">
           <thead className="cmp-current-projects__table-head">
             <tr className="cmp-current-projects__table-header-row">
-              <th scope="col" className="cmp-current-projects__table-header-cell">Project</th>
               <th scope="col" className="cmp-current-projects__table-header-cell">Client</th>
+              <th scope="col" className="cmp-current-projects__table-header-cell">Project</th>
               <th scope="col" className="cmp-current-projects__table-header-cell">Start</th>
               <th scope="col" className="cmp-current-projects__table-header-cell">End</th>
             </tr>
