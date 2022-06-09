566d28fc274b0ef3cd763a76dbb5c43ce1f9334e
Emma Litwa-Vulcu <emma@heysparkbox.com>
2022-06-09 14:22:23 -0700

    feat: added clipboard copy button to bio
    renamed __header to __heading for clarity/consistence
    __header is now flexbox containing __heading and button
    
    FSA22V1-241

--- components/PersonalBio.jsx
+++ components/PersonalBio.jsx
@@ -1,14 +1,18 @@
+import CopyToClipboardButton from './CopyToClipboardButton';
 import Show from './Show';
 
 export default function PersonalBio({ name, bio }) {
   return (
     <Show when={bio}>
       <section aria-labelledby="personal-bio" className="cmp-personal-bio">
-        <h3 id="personal-bio" className="cmp-personal-bio__header">
-          About
-          {' '}
-          {name}
-        </h3>
+        <div className="cmp-personal-bio__header">
+          <h3 id="personal-bio" className="cmp-personal-bio__heading">
+            About
+            {' '}
+            {name}
+          </h3>
+          <CopyToClipboardButton content={bio} label="Copy Bio" />
+        </div>
         <p className="cmp-personal-bio__content">
           {bio}
         </p>
