1f50752d2ed51fb04ff82b304d4c309ff9b84e81
Emma Litwa-Vulcu <emma@heysparkbox.com>
2022-06-10 09:27:24 -0700

    fix: lint now passes, tests pass
    - linting fixes
    - tests pass
    
    FSA22V1-241

--- components/CopyToClipboardButton.jsx
+++ components/CopyToClipboardButton.jsx
@@ -1,33 +1,34 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 // Button that copies given text `content` to the users clipboard.
 // `label` and `labelCopied` are the text to display before/after click.
-const CopyToClipboardButton = ({ content, label, labelCopied }) => {
-  const [copied, setCopied] = useState(false)
+function CopyToClipboardButton({ content, label, labelCopied }) {
+  const [copied, setCopied] = useState(false);
 
   const copyContent = (e) => {
-    e.preventDefault()
-    navigator.clipboard.writeText(content)
-    setCopied(true)
+    e.preventDefault();
+    navigator.clipboard.writeText(content);
+    setCopied(true);
     setTimeout(() => {
-      setCopied(false)
-    }, 3000)
-  }
+      setCopied(false);
+    }, 3000);
+  };
 
   return (
     <button
+      type="button"
       onClick={copyContent}
       className="cmp-btn cmp-btn__link cmp-copy-to-clipboard-button"
     >
       {copied ? labelCopied : label}
     </button>
-  )
+  );
 }
 
 CopyToClipboardButton.defaultProps = {
   content: '',
   label: 'Copy Text',
-  labelCopied: 'Copied!'
-}
+  labelCopied: 'Copied!',
+};
 
-export default CopyToClipboardButton
+export default CopyToClipboardButton;
