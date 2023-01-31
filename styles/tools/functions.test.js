234669388781849aacad457cb7f75cb30c759ec6
dustin-jw <dustin@heysparkbox.com>
2023-01-31 14:43:40 -0700

    chore: delete sass tests
    The updated version of `sass-true` broke the tests, and rather than
    spending the time troubleshooting, this deletes the tests, which are
    nice-to-have at best.

--- styles/tools/functions.test.js
+++
@@ -1,9 +0,0 @@
-/**
- * @jest-environment node
- */
-
-import { join } from 'path';
-import { runSass } from 'sass-true';
-
-const sassFile = join(__dirname, 'functions_test.scss');
-runSass({ file: sassFile }, { describe, it });
